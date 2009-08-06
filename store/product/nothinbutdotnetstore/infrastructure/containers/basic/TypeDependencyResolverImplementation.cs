53284cd1c13b28b255f3d7d8f0cff41a4d071d27
David Tchepak <dave@davesquared.net>
2009-08-06 14:18:31 +1000

    Implemented something

--- store/product/nothinbutdotnetstore/infrastructure/containers/basic/TypeDependencyResolverImplementation.cs
+++ store/product/nothinbutdotnetstore/infrastructure/containers/basic/TypeDependencyResolverImplementation.cs
@@ -1,19 +1,24 @@
 using System;
+using System.Collections.Generic;
 
 namespace nothinbutdotnetstore.infrastructure.containers.basic
 {
     public class TypeDependencyResolverImplementation : TypeDependencyResolver
     {
+        IDictionary<Type, Type> mappings = new Dictionary<Type, Type>();
+
         public T resolve_concrete_type<T>() {
-            throw new NotImplementedException();
+            return (T) resolve_concrete_type(typeof(T));
 
         }
         public object resolve_concrete_type(Type type_of_class_to_resolve)
         {
-            throw new NotImplementedException();
+            var type_to_create = mappings[type_of_class_to_resolve];
+            return Activator.CreateInstance(type_to_create);
         }
 
         public void add_mapping(Type map_from, Type map_to) { 
+            mappings.Add(map_from, map_to);
         }
     }
 }
