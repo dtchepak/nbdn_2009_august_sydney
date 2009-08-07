272d4200fcdf95010a8c5045e6df69f016880576
Anthony Egerton <github@delfish.com>
2009-08-07 18:49:00 +1000

    Add button binder

---
+++ store/product/nothinbutdotnetstore.tests/ui/ButtonBinderSpec.cs
@@ -0,0 +1,45 @@
+using System;
+using developwithpassion.bdd;
+using nothinbutdotnetstore.infrastructure;
+using developwithpassion.bdd.contexts;
+using developwithpassion.bdd.mbunit.standard.observations;
+using developwithpassion.bdddoc.core;
+using nothinbutdotnetstore.ui;
+using developwithpassion.bdd.mbunit;
+using System.Windows.Forms;
+using Rhino.Mocks;
+
+namespace build.product.docs
+{
+    public class ButtonBinderSpec
+    {
+        public abstract class concern : observations_for_a_sut_without_a_contract<ButtonBinder>
+        {
+
+        }
+
+        [Concern(typeof(ButtonBinder))]
+        public class when_button_is_clicked : concern
+        {
+            context c = () =>
+            {
+                button = new Button();
+                provide_a_basic_sut_constructor_argument(button);
+                command = the_dependency<Command>();
+            };
+
+            because b = () =>
+            {
+                button.PerformClick();
+            };
+
+            it should_run_command = () =>
+            {
+                command.received(x => x.run());
+            };
+
+            static Button button;
+            static Command command;
+        }
+    }
+}
---
+++ store/product/nothinbutdotnetstore/ui/ButtonBinder.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Windows.Forms;
+using nothinbutdotnetstore.infrastructure;
+
+namespace nothinbutdotnetstore.ui
+{
+    public class ButtonBinder
+    {
+        public ButtonBinder(Button button, Command command)
+        {
+            button.Click += (sender, args) => command.run();
+        }
+    }
+}
