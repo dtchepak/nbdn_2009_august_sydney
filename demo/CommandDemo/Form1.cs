53652ca07b3e139965143877615cc6a27ed40f2b
David Tchepak <dave@davesquared.net>
2009-08-08 00:39:25 +1000

    Gave up

---
+++ demo/CommandDemo/Form1.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows.Forms;
+using nothinbutdotnetstore.ui;
+
+namespace CommandDemo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            new ButtonBinder(button1, new SayHelloWorldCommand(s => label1.Text = s));
+            new ButtonBinder(button2, new EchoTextCommand(() => textBox1.Text, s => label2.Text = s));
+            new ButtonBinder(button3, new ListAdderCommand(listView1, () => textBox1.Text));
+        }
+    }
+}
